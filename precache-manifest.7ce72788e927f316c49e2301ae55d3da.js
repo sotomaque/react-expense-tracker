4b9f77f5c4df5af0364e9a2f7ebc1df161c67777
sotomaque <sotomaque@gmail.com>
2020-02-26 09:56:18 -0800

    Updates

---
+++ precache-manifest.7ce72788e927f316c49e2301ae55d3da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801d7a6d4dc7bb4d95bfbafc9af7950e",
+    "url": "/react-expense-tracker/index.html"
+  },
+  {
+    "revision": "57783661e5b9e1203641",
+    "url": "/react-expense-tracker/static/css/main.6ebd63e0.chunk.css"
+  },
+  {
+    "revision": "440b776fd947060527ec",
+    "url": "/react-expense-tracker/static/js/2.954b3d2b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-expense-tracker/static/js/2.954b3d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57783661e5b9e1203641",
+    "url": "/react-expense-tracker/static/js/main.6c27b466.chunk.js"
+  },
+  {
+    "revision": "7cdbd22dae43328efd96",
+    "url": "/react-expense-tracker/static/js/runtime-main.96cda5c9.js"
+  }
+]);
